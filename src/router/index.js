9a05054eb056888491e7dc1d51ded935b1b7a92f
KritT9 <kritsadaboonprakong@gmail.com>
2025-09-05 11:46:12 +0700

    update v2

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,16 @@ import Home from "../views/HomeView.vue";
 import Menu from "../components/Menu.vue";
 import ShowProduct from "../views/ShowProducts.vue";
 import About from "../views/AboutView.vue";
+import Customer from "../views/customer.vue";
+
 
 
 const routes = [
   { path: "/", name: "Home", component:  Home },
   { path: "/showproduct", name: "ShowProduct", component: ShowProduct },
   { path: "/about", name: "about", component: About},
-  { path: "/menu", name: "menu", component: Menu}
+  { path: "/menu", name: "menu", component: Menu},
+  { path: "/customer", name: "customer", component: Customer}
 ]
 
 const router = createRouter({
